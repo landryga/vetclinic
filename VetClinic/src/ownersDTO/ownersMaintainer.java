12df6bc9dd7acfae6e3acaa58b5c63d3aef4fc58
landryga <kuba.walenda@gmail.com>
2017-03-26 13:18:47 +0200

    Loading first java classes

---
+++ VetClinic/src/ownersDTO/ownersMaintainer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ownersDTO;
+
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Kuba
+ */
+public class ownersMaintainer {
+    
+    private JdbcTemplate jdbcTemplate;
+    protected String OwnerName;
+    protected String OwnerSurname;
+    protected String OwnerAddress;
+    protected String OwnerTelephone;
+    
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+}
